ef17ba0ff65a5158e7f4509606c6ce54bafcf38f
vuhger <victor.uhnger@gmail.com>
2025-12-28 09:18:57 +0100

    update page content

--- components/home/BentoGrid.tsx
+++ components/home/BentoGrid.tsx
@@ -45,7 +45,6 @@ const BentoGrid = () => {
                 Jeg er interessert i distribuerte systemer og nettverk. 
               </Paragraph>
               <Paragraph data-size="xs" style={{ color: 'var(--ds-color-neutral-text-default)', maxWidth: '36rem', margin: 0 }}>
-                Jeg søker ikke aktivt jobb nå, men ta gjerne kontakt hvis profilen min er interessant. :)
               </Paragraph>
               <Paragraph data-size="xs" style={{ color: 'var(--ds-color-neutral-text-default)', maxWidth: '36rem', margin: 0 }}>
                 Liker også automatisering og KI!
