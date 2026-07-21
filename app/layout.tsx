4a0b40acdd18cddd19823ef648b73936c07f2035
vuhger <victor.uhnger@gmail.com>
2026-07-21 20:03:07 +0200

    fix: suppress body hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </head>
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         style={{
           backgroundColor: 'var(--ds-color-neutral-background-default)',
